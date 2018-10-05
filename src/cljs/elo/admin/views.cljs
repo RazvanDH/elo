fc1cc1b33c2f741efa21c3e551655f1ad972de8f
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-10-05 16:31:21 +0100

    extra indirection

--- src/cljs/elo/admin/views.cljs
+++ src/cljs/elo/admin/views.cljs
@@ -11,32 +11,34 @@
         league (rf/subscribe [::handlers/league])
         leagues (rf/subscribe [::handlers/leagues])]
 
-    (js/console.log "Leagues are now =" @leagues)
-    [:div.form-group.add-player_form
-     [:div
-      [drop-down @leagues :league]
+    (fn []
+      (js/console.log "Leagues are now =" @leagues)
+      [:div.form-group.add-player_form
+       [:div
+        [drop-down @leagues :league]
 
-      [:input.form-control {:type "text"
-                            :value (:name @player)
-                            :name "name"
-                            :placeholder "John Smith"
-                            :on-change (utils/set-val :name)}]
+        [:input.form-control {:type "text"
+                              :value (:name @player)
+                              :name "name"
+                              :placeholder "John Smith"
+                              :on-change (utils/set-val :name)}]
 
-      [:input.form-control {:type "text"
-                            :value (:email @player)
-                            :name "email"
-                            :placeholder "john.smith@email.com"
-                            :on-change (utils/set-val :email)}]]
+        [:input.form-control {:type "text"
+                              :value (:email @player)
+                              :name "email"
+                              :placeholder "john.smith@email.com"
+                              :on-change (utils/set-val :email)}]]
 
-     [:div
-      [:button {:type "button"
-                :name "submit-game"
-                :class (utils/classes ["submit__game" "btn" "btn-primary" (when-not @valid-player? "disabled")])
-                :on-click (if @valid-player?
-                            #(rf/dispatch [::handlers/add-player])
-                            #(js/alert "Fill up the form first"))}
+       [:div
+        [:button {:type "button"
+                  :name "submit-game"
+                  :class (utils/classes ["submit__game" "btn" "btn-primary" (when-not @valid-player? "disabled")])
+                  :on-click (if @valid-player?
+                              #(rf/dispatch [::handlers/add-player])
+                              #(js/alert "Fill up the form first"))}
 
-       "Register New Player"]]]))
+         "Register New Player"]]])))
+
 
 (defn root
   []
